2c4d7ee4e28a3b78b99e6e63d39b70c799f7741a
WaterKat <waterkat@aonyxlimited.com>
2025-01-19 18:52:36 -0800

    added new configuration fields for text to speech

--- src/fields.ts
+++ src/fields.ts
@@ -98,8 +98,12 @@ export type Field = {
     | FieldWidgetDuration;
 };
 
-type GenericFieldData<T extends Field, K extends keyof T> = {
-  [key in K]: T[key] extends { value: infer P } ? P : never;
+type GenericFieldData<T extends Field, T_K extends keyof T> = {
+  [K in T_K]: T[K] extends { value: infer P }
+    ? P
+    : T[K] extends FieldImage | FieldVideo | FieldSound
+    ? string
+    : never;
 };
 
 type AssertIsField<T extends Field> = T;
@@ -109,6 +113,27 @@ const rawConfiguration = {
   widgetName: {
     type: "hidden",
     value: "AonyxBuddy",
+  },
+  introductionMessage: {
+    type: "text",
+    label: "Message that plays on intialization",
+    value: "Hello, this is AonyxBuddy",
+  },
+  TTSWaitTime: {
+    type: "number",
+    label: "Time between TTS messages",
+    value: 0.5,
+    min: 0,
+    max: 10000,
+    step: 0.1,
+  },
+  baseSprite: {
+    type: "image-input",
+    label: "Base Sprite",
+  },
+  talkingSprite: {
+    type: "image-input",
+    label: "Talking Sprite",
   },
 } as const;
 
