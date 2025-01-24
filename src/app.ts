492c98371cb9bff34e99c4261d689d4c7376cd23
WaterKat <waterkat@aonyxlimited.com>
2025-01-23 17:14:17 -0800

    added listener for stream events

--- src/app.ts
+++ src/app.ts
@@ -7,22 +7,34 @@ import { GetTextResponse, ProcessStreamElementsEvent } from "./events.js";
 function main(options: OnWidgetLoadData) {
   //! allow access to the options globally
   window.aonyxbuddy = options;
+  if (window.aonyxbuddy?.overlay.isEditorMode) {
+    console.log("AonyxBuddy Options", options);
+  }
 
   const audio = new AudioPlayer({
     waitTime: options.fieldData.TTSWaitTime * 1000,
     retryTime: 100,
   });
 
-  audio.queue(new URL(GetSETTSURL("Brian", "Hello world!")));
-  audio.queue(new URL(GetSETTSURL("Brian", "Hello world! One")));
-  audio.queue(new URL(GetSETTSURL("Brian", "Hello world! Two")));
-  audio.queue(new URL(GetSETTSURL("Brian", "Hello world! Three")));
-  audio.play();
+  const welcomeMessage = GetStreamElementsTTSUrl(
+    options.fieldData.TTSVoice,
+    options.fieldData.introductionMessage
+  );
+  if (welcomeMessage) {
+    audio.queue(new URL(welcomeMessage));
+    audio.play();
+  } else {
+    if (window.aonyxbuddy?.overlay.isEditorMode) {
+      console.error("No welcome message found");
+    }
+  }
 
+
+  const b = new URL(options.fieldData.baseSprite);
   const renderer = new SpriteRenderer({
     parent: document.getElementById("app") ?? document.body,
     spriteURLs: {
-      base: new URL(options.fieldData.baseSprite),
+      base: b,
       talking: new URL(options.fieldData.talkingSprite),
     },
   });
@@ -34,11 +46,44 @@ function main(options: OnWidgetLoadData) {
     params.base %= 1;
     params.talking = audio.amplitude();
   }, 1000 / 24);
-}
 
-window.addEventListener("onEventReceived", (rawEvent) => {
-  console.info("onEventReceived", rawEvent);
-});
+  window.addEventListener("onEventReceived", (rawEvent: Event) => {
+    if (
+      !rawEvent ||
+      !(rawEvent as any).detail ||
+      !(rawEvent as any).detail.event
+    ) {
+      if (window.aonyxbuddy?.overlay.isEditorMode) {
+        console.log("No event found", rawEvent);
+      }
+      return;
+    }
+
+    const event: any = (rawEvent as any).detail.event;
+
+    if (window.aonyxbuddy?.overlay.isEditorMode) {
+      console.log("Raw Event", event);
+    }
+
+    const processedEvent = ProcessStreamElementsEvent(event);
+    const textResponse = GetTextResponse(processedEvent);
+
+    const response = GetStreamElementsTTSUrl(
+      options.fieldData.TTSVoice,
+      textResponse
+    );
+
+    if (!response) {
+      if (window.aonyxbuddy?.overlay.isEditorMode) {
+        console.error("No response found");
+      }
+      return;
+    }
+
+    audio.queue(response);
+    audio.play();
+  });
+}
 
 window.addEventListener("onWidgetLoad", (raw: unknown) => {
   if (!raw || !(raw as any).detail) {
